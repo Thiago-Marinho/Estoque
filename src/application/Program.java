b3b15a8e312f6a898ed94a74b9c24cc777826dec
Thiago-Marinho <peres.marinho@gmail.com>
2021-07-22 18:05:56 -0400

    Lista os produtos registrados mostrando suas informações mais data e o horários que foram registados

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
@@ -9,10 +11,11 @@ import entitie.Product;
 
 public class Program {
 
-	public static void main(String[] args) {	
+	public static void main(String[] args)  {			
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat date = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 		
 		System.out.print("how many products do you want to register?");
 		int n = sc.nextInt();
@@ -30,29 +33,42 @@ public class Program {
 		System.out.print("Quantity: ");
 		quantity=sc.nextInt();
 		
-		Product pdt = new Product(name, price , quantity);
+		Date x = new Date();
 		
+		Product pdt = new Product(name, price , quantity, x);		
 		list.add(pdt);
+				
 		}		
 		
+		
 		for (Product pdt : list) {
-		System.out.println("_______________________________________________________________________");
-		System.out.println("\nProduct data: "+ pdt);
-		
-		System.out.print("\nEnter the number of products to be added in stock: ");
-		
-		quantity = sc.nextInt();
-		pdt.addProduct(quantity);
-		
-		System.out.println("\nUpdated data: "+ pdt);
-		
-		System.out.print("\nEnter the number of products to be removed from stock: ");
-		
-		quantity = sc.nextInt();
-		pdt.removeProduct(quantity);
-		
-		System.out.println("\nUpdated data: "+ pdt);
+			System.out.println("_______________________________________________________________________");
+			System.out.println("\nProduct data: "+ pdt);
+			
+			System.out.print("\nEnter the number of products to be added in stock: ");
+			
+			quantity = sc.nextInt();
+			pdt.addProduct(quantity);
+			
+			System.out.println("\nUpdated data: "+ pdt);
+			
+			System.out.print("\nEnter the number of products to be removed from stock: ");
+			
+			quantity = sc.nextInt();
+			pdt.removeProduct(quantity);
+			
+			System.out.println("\nUpdated data: "+ pdt);
 		}
+		
+		System.out.println("\nList of all products:");
+		for(Product pdt : list) {
+			
+			System.out.println(	"Name: "+ pdt.getName() +"; "+
+								"Price: "+ pdt.getPrice() +"; "+
+								"Quantity: "+ pdt.getQuantity() +"; "+
+								"Date/Hour: "+date.format(pdt.getDate()));
+		}
+		
 		
 		sc.close();
 	}
--- src/entitie/Product.java
+++ src/entitie/Product.java
@@ -1,21 +1,26 @@
 package entitie;
 
+import java.util.Date;
+
 public class Product {
 
 	private String name;
 	private double price;
 	private int quantity;
-	
+	private Date date;
 	//Construtor e Sobrecarga
 	
 	public Product () {}
 	
-	public Product(String name, double price, int quantity) {
+	public Product(String name, double price, int quantity, Date date) {
 		this.name = name;
 		this.price = price;
 		this.quantity = quantity;
+		this.date = date;
 	}
 	
+	
+
 	public Product(String name, double price) {
 		this.name = name;
 		this.price = price;		
@@ -37,6 +42,13 @@ public class Product {
 		return quantity;
 	}	
 	
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
 	public double totalValue() {
 		return (price * quantity);
 	}
